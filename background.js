b2b9bed160f7abea27ef85326988a0a7ce47c0ec
Brad Dettmer <bd@Brads-MacBook-Pro.local>
2016-09-24 14:01:40 -0400

    Making word replace work.

--- background.js
+++ background.js
@@ -1,1 +1,22 @@
-alert('hello');
+alert('hello twitter TESTING!!');
+
+
+
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.replace(/the/gi, 'IT WORKS!!!!!');
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
