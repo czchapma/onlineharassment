334287709a2c3dd9b9c4b0980d50dfd118dba19a
Bejal Lewis <bejallewis@hotmail.com>
2016-09-24 15:02:46 -0400

    add mappings for bad words

--- background.js
+++ background.js
@@ -1,7 +1,3 @@
-alert('hello twitter TESTING!!');
-
-
-
 var elements = document.getElementsByTagName('*');
 
 for (var i = 0; i < elements.length; i++) {
@@ -12,7 +8,10 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/the/gi, 'IT WORKS!!!!!');
+            var replacedText = text.replace(/\sthe\s+/gi, 'IT WORKS!!!!!');
+            replacedText = replacedText.replace(/\shate\s/gi, ' love ');
+            replacedText = replacedText.replace(/badword1/gi, ' All my friends have birthdays this year! ');
+            replacedText = replacedText.replace(/badword2/gi, ' I\'m pedaling backward!');
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
