a50fc23f7a087a437a70fd4ed0329974f1b1cf8b
Christine <christine_chapman@brown.edu>
2016-09-24 22:50:31 -0400

    Removed test debugging statement from DOM parsing

--- background.js
+++ background.js
@@ -8,8 +8,7 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/\sthe\s+/gi, 'IT WORKS!!!!!');
-            replacedText = replacedText.replace(/\shate\s/gi, ' love ');
+            var replacedText = text.replace(/\shate\s/gi, ' love ');
             replacedText = replacedText.replace(/badword1/gi, ' All my friends have birthdays this year! ');
             replacedText = replacedText.replace(/badword2/gi, ' I\'m pedaling backward!');
 
