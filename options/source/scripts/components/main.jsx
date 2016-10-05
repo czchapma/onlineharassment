90dc264e4ca17d695da7e662e81970f94bd62496
Christine Liao <astridcliao@gmail.com>
2016-10-05 10:18:40 -0700

    added simple main react component to options

---
+++ options/source/scripts/components/main.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Stop Harassment Word Settings</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
