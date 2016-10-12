3902f2fe4f4a49d4f132074781b3bc17e0886631
Christine Liao <astridcliao@gmail.com>
2016-10-11 18:41:05 -0700

    took away messaging/fixed minor bug

--- content/source/scripts/index.js
+++ content/source/scripts/index.js
@@ -35,8 +35,8 @@ const filter = function() {
   }
 }
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
     proxyStore.subscribe(filter);
-  }
-);
+  // }
+// );
