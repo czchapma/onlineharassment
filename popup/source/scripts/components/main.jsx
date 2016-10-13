3b98e93080bba331d56cbfc0fafd7579262c1643
Christine <christine_chapman@brown.edu>
2016-10-13 00:56:15 -0400

    Added new quotes and the ability to switch between them

--- popup/source/scripts/components/main.jsx
+++ popup/source/scripts/components/main.jsx
@@ -12,10 +12,12 @@ class App extends Component{
   render() {
     let filter_status = (this.props.filter_on) ? 'enabled' : 'disabled';
     let filter_options = (this.props.filter_on) ? <FilterOptions checkFilterOption={this.props.checkFilterOption} filter_options={this.props.filter_options}/> : <div></div>;
+    let quotes = ["'Kind words can be short and easy to speak but their echoes are truly endless.' --Mother Theresa", "'You are braver than you believe, stronger than you seem, and smarter than you think.' --Christopher Robin", "'Be who you are and say what you feel, because those who mind don't matter and those who matter don't mind.' -- Dr. Seuss", "'One must always be careful of books and what is inside them, for words have the power to change us.' - Tessa Gray", "'Don't let the Muggles get you down.' --Ron Weasley"];
+    let inspirational_quote = quotes[Math.floor(Math.random() * quotes.length)];
     return (
       <div>
         <h1>Stop Harassment</h1>
-        <h5>'Kind words can be short and easy to speak but their echoes are truly endless.' --Mother Theresa</h5>
+        <h5>{inspirational_quote}</h5>
         <div>
           <h3>Word Filtering</h3>
           <Switch onClick={this.props.toggleFilter} on={this.props.filter_on}/>
