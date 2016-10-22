9fb5c2f485913cbc9f962403c15127e903175e3a
Christine Liao <astridcliao@gmail.com>
2016-10-21 17:48:59 -0700

    deleted excess comments/functions

--- content/source/scripts/index.js
+++ content/source/scripts/index.js
@@ -4,12 +4,10 @@ const proxyStore = new Store({
   portName: 'STOP_HARASSMENT'
 });
 
-//Removes the tweet entirely, previously known as filterAndRemove
-//trying to refactor to write less code/combine
 const filterOnType = function() {
   let state = proxyStore.getState();
   let harmful_words = state.harmful_words;
-  let filter_options = state.filter_options; //testing
+  let filter_options = state.filter_options;
   let filter_on = state.filter_on;
 
   var elements = document.getElementsByClassName('tweet');
@@ -62,60 +60,9 @@ const filterOnType = function() {
   }
 }
 
-//undoes filterAndRemove
-const undoFilterAndRemove = function(){
-  let state = proxyStore.getState();
-  let harmful_words = state.harmful_words;
-
-  var elements = document.getElementsByClassName('tweet');
-
-  for (var i = 0; i < elements.length; i++) {
-    var tweetElement = elements[i];
-    var text = tweetElement.getElementsByClassName('tweet-text')[0];
-    if (text) {
-      harmful_words.forEach( word => {
-        var regex = new RegExp(word, "gi");
-        if (regex.test(text.textContent)) {
-          tweetElement.style.visibility = "visible";
-        }
-      });
-    }
-  }
-}
-
-//Replaces the tweet with a nice phrase
-const filterAndReplace = function() {
-  let state = proxyStore.getState();
-  let harmful_words = state.harmful_words;
-
-  var elements = document.getElementsByTagName('*');
-
-  for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-    for (var j = 0; j < element.childNodes.length; j++) {
-      var node = element.childNodes[j];
-      console.log(node);
-      if (node.nodeType === 3) {
-        var text = node.nodeValue;
-        harmful_words.forEach( word => {
-          var regex = new RegExp(word, "gi");
-          var replacedText = text.replace(regex, 'I bet you sweat glitter!');
-          if (replacedText !== text) {
-            element.replaceChild(document.createTextNode(replacedText), node);
-          }
-        })
-      }
-    }
-  }
-}
-
 const filter = function(){
-  // let state = proxyStore.getState();
-
-  // if (state.filter_on) {
-    filterOnType();
-    setInterval(filterOnType, 1000);
-  // };
+  filterOnType();
+  setInterval(filterOnType, 1000);
 }
 
 proxyStore.subscribe(filter);
