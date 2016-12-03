ab919b3e4dd00af6a50192f62dd04ba29c67f6be
Christine Liao <astridcliao@gmail.com>
2016-12-03 13:27:21 -0800

    caching implemented to reduce ajax calls

--- content/source/scripts/index.js
+++ content/source/scripts/index.js
@@ -5,6 +5,7 @@ const proxyStore = new Store({
   portName: 'STOP_HARASSMENT'
 });
 
+//caching hateful tweets
 let hateful_tweet_ids = [];
 const filterOnType = function() {
   let state = proxyStore.getState();
@@ -16,44 +17,51 @@ const filterOnType = function() {
   for (var i = 0; i < elements.length; i++) {
     var tweetElement = elements[i];
     var tweetId = tweetElement.getAttribute('data-tweet-id');
-    var text = tweetElement.getElementsByClassName('tweet-text')[0];
-    if (text) {
 
-      harmful_words.forEach( word => {
-        var regex = new RegExp(word, "gi");
+    if (hateful_tweet_ids.includes(tweetId)){
+      tweetElement.style.display = "none";
+    } else {
+      var text = tweetElement.getElementsByClassName('tweet-text')[0];
+      if (text) {
 
-        var text_content = text.textContent;
+        harmful_words.forEach( word => {
+          var regex = new RegExp(word, "gi");
 
-        //if tweet contains harmful word
-        if (regex.test(text_content)) {
+          var text_content = text.textContent;
 
-          //if filter is off
-          if (!filter_on) {
-            tweetElement.style = "inherit";
+          //if tweet contains harmful word
+          if (regex.test(text_content)) {
 
-          //hiding tweets if negative sentiment
-          } else {
-            $.ajax({
-              url: "https://localhost:3000/",
-              type: "POST",
-              data: { text: text_content, tweet_id: tweetId },
-              // async: false,
-              success: function (res) {
-                if (res.negative){
-                  hateful_tweet_ids.push(res.tweet_id);
-                  var badTweet = $("[data-tweet-id=" + res.tweet_id + "]")[0];
-                  badTweet.style.display = "none";
-                  console.log(badTweet);
+            //if filter is off
+            if (!filter_on) {
+              tweetElement.style = "inherit";
+
+              //hiding tweets if negative sentiment
+            } else {
+              $.ajax({
+                url: "https://localhost:3000/",
+                type: "POST",
+                data: { text: text_content, tweet_id: tweetId },
+                // async: false,
+                success: function (res) {
+                  if (res.negative){
+                    hateful_tweet_ids.push(res.tweet_id);
+                    var badTweet = $("[data-tweet-id=" + res.tweet_id + "]")[0];
+                    badTweet.style.display = "none";
+                    console.log(hateful_tweet_ids);
+                  }
                 }
-              }
-            });
-          };
+              });
+            };
 
-        }
+          }
 
-      });
+        });
+
+      }
 
     }
+
   }
 }
 
