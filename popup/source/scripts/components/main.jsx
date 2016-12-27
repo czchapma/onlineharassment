f187e6cbcebdc62091d60fb44603ea5e0c02a26c
Seunglee Lee <seung_lee_lee@brown.edu>
2016-12-27 00:58:15 -0500

    added additional quotes on the extension page

--- popup/source/scripts/components/main.jsx
+++ popup/source/scripts/components/main.jsx
@@ -18,7 +18,7 @@ class App extends Component{
 
   render() {
     let filter_status = (this.props.filter_on) ? 'enabled' : 'disabled';
-    let quotes = ["'Kind words can be short and easy to speak but their echoes are truly endless.' --Mother Theresa", "'You are braver than you believe, stronger than you seem, and smarter than you think.' --Christopher Robin", "'Be who you are and say what you feel, because those who mind don't matter and those who matter don't mind.' -- Dr. Seuss", "'One must always be careful of books and what is inside them, for words have the power to change us.' - Tessa Gray", "'Don't let the Muggles get you down.' --Ron Weasley"];
+    let quotes = ["'Kind words can be short and easy to speak but their echoes are truly endless.' --Mother Theresa", "'You are braver than you believe, stronger than you seem, and smarter than you think.' --Christopher Robin", "'Be who you are and say what you feel, because those who mind don't matter and those who matter don't mind.' -- Dr. Seuss", "'One must always be careful of books and what is inside them, for words have the power to change us.' - Tessa Gray", "'Don't let the Muggles get you down.' --Ron Weasley", "'Try to be a rainbow in someone's cloud.' --Maya Angelou", "'No act of kindness, no matter how small, is ever wasted.' --Aesop", "'I have found that if you love life, life will love you back.' --Arthur Rubinstein", "'The best thing to hold onto in life is each other.' --Audrey Hepburn", "'Love is the only force capable of transforming an enemy into a friend.' --Martin Luther King, Jr."];
     let inspirational_quote = quotes[Math.floor(Math.random() * quotes.length)];
     return (
       <div>
