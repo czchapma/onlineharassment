0866907f37bb50eaf1b46f5bd0fc0212136c3a41
Bejal Lewis <bejallewis@hotmail.com>
2016-12-30 17:46:50 +0000

    edited transitions and search bar width

--- options/source/scripts/components/main.jsx
+++ options/source/scripts/components/main.jsx
@@ -23,6 +23,20 @@ class App extends Component{
     this.props.removeWord(e.target.value);
   }
 
+  showCross(element){
+    var reference = element["i"];
+    this.refs[reference].style.visibility = "visible";
+  }
+
+  hideCross(element){
+    var reference = element["i"];
+    this.refs[reference].style.visibility = "hidden";
+  }
+
+  iToString(i){
+    return i + '';
+  }
+
   render(){
     let harmful_words = this.props.harmful_words;
     let word_list = this.props.harmful_words || [];
@@ -33,7 +47,7 @@ class App extends Component{
     <ul id="autocompleteList">
       {filtered_words.map((word, i) => {
         return (
-            <li className="wordItem" key={i}>{word}<button className="removeButton pull-right" value={word} onClick={this.removeWord.bind(this)}>X</button></li>);
+            <li className="wordItem" onMouseOut={this.hideCross.bind(this, {i})} onMouseOver={this.showCross.bind(this, {i})} key={i}>{word}<button style={{visibility : "hidden"}} ref={i} className="removeButton pull-right" value={word} onClick={this.removeWord.bind(this)}>X</button></li>);
       })}
     </ul>
     return (
@@ -45,8 +59,8 @@ class App extends Component{
           <div id="mainContent">
             <h4 id="wordList">Word List</h4>
             <div className="row fullRow" id="listInputs">
-              <input type="text" id="wordInput" className="col-sm-6 pull-left" placeholder='Add Word and Press Enter' onKeyPress={this.addWord.bind(this)} />
-              <input id="search" type="text" className="col-sm-6 pull-right" placeholder="Search" valueLink={linkState(this, 'inputVal')} />
+              <input type="text" id="wordInput" className="col-sm-12 col-md-6 pull-left" placeholder='Add Word and Press Enter' onKeyPress={this.addWord.bind(this)} />
+              <input id="search" type="text" className="col-sm-12 col-md-6 pull-right" placeholder="Search" valueLink={linkState(this, 'inputVal')} />
             </div>
             <div id="list">{autocomplete}</div>
           </div>
