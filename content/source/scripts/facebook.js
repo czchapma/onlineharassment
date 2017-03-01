0b9687d65dcb64ca2b1dff0c5c411d45c1ddd190
Christine Liao <astridcliao@gmail.com>
2017-03-01 10:33:26 -0800

    setting up facebook skeleton

---
+++ content/source/scripts/facebook.js
@@ -0,0 +1,9 @@
+import { contains_misspelling } from './jaro_winkler';
+
+const checkFacebookFilter = function(state){
+  const harmful_words = state.harmful_words;
+  const filter_on = state.filter_on;
+
+};
+
+export default checkFacebookFilter;
--- content/source/scripts/index.js
+++ content/source/scripts/index.js
@@ -1,6 +1,7 @@
 import {Store} from 'react-chrome-redux';
 import checkTwitterFilter from './twitter';
 import checkYoutubeFilter from './youtube';
+import checkFacebookFilter from './facebook';
 
 const proxyStore = new Store({
   portName: 'STOP_HARASSMENT'
@@ -10,11 +11,14 @@ const filter = function(){
   const url = window.location.href;
   const youtube = /youtube\.com/;
   const twitter = /twitter\.com/;
+  const facebook = /facebook\.com/;
   const state = proxyStore.getState();
   if (youtube.test(url)){
     checkYoutubeFilter(state);
   } else if (twitter.test(url)){
     checkTwitterFilter(state);
+  } else if (facebook.test(url)){
+    checkFacebookFilter(state);
   }
   // commented out to prevent exceeding daily limit of express https server
   // setInterval(filterOnType, 1000);
