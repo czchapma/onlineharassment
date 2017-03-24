5f88063d184b76dbdce1974012542f05d0a5b53d
Christine Liao <astridcliao@gmail.com>
2017-03-24 15:51:32 -0700

    hiding my facebook comment

--- content/source/scripts/facebook.js
+++ content/source/scripts/facebook.js
@@ -1,7 +1,7 @@
 import { contains_misspelling } from './jaro_winkler';
 console.log('in facebook');
 
-const negative_comments = ['u_0_2t'];
+const negative_comments = [];
 const positive_comments = [];
 
 const checkFacebookFilter = function(state){
@@ -15,9 +15,35 @@ const checkFacebookFilter = function(state){
       post.style.display = 'none';
     } else if (positive_comments.includes(postId)){
       console.log('already deemed positive');
+    } else {
+      let postContent = document.getElementsByClassName('userContent')[0].innerHTML;
+      console.log(postContent);
+      harmful_words.forEach(word => {
+        let regex = new RegExp(word, "gi");
+
+        if (regex.test(postContent) || contains_misspelling(postContent, word)) {
+          let xhr = new XMLHttpRequest();
+          let data = `text=${postContent}&comment_id=${postId}&username=""`;
+          xhr.open('POST', "https://localhost:3000/");
+          xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+          xhr.onload = function() {
+            if (xhr.status == 200) {
+              let res = xhr.responseText;
+              let jsonResponse = JSON.parse(res);
+              if (jsonResponse.negative){
+                negative_comments.push(jsonResponse.comment_id);
+                let badComment = document.querySelectorAll(`[id="${jsonResponse.comment_id}"]`)[0];
+                badComment.style.display = "none";
+                console.log(negative_comments);
+              } else {
+                positive_comments.push(jsonResponse.comment_id);
+              }
+            }
+          };
+          xhr.send(data);
+        }
+      })
     }
-    let postContent = document.getElementsByClassName('userContent')[0].innerHTML;
-    console.log(postContent);
   })
 };
 
