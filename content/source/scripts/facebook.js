e7b810855a75ce3377b240d9cd6faecf1283c667
Christine Liao <astridcliao@gmail.com>
2017-03-24 15:38:14 -0700

    post hiding with id

--- content/source/scripts/facebook.js
+++ content/source/scripts/facebook.js
@@ -1,14 +1,23 @@
 import { contains_misspelling } from './jaro_winkler';
+console.log('in facebook');
 
-const negative_comments = [];
+const negative_comments = ['u_0_2t'];
 const positive_comments = [];
 
 const checkFacebookFilter = function(state){
   const harmful_words = state.harmful_words;
   const filter_on = state.filter_on;
-  let posts = Array.from(document.getElementsByClassName('UserContent'));
+  let posts = Array.from(document.getElementsByClassName('userContentWrapper'));
   posts.forEach(post => {
-    
+    let postId = post.parentNode.getAttribute('id');
+    console.log(postId, postId === negative_comments[0]);
+    if (negative_comments.includes(postId)){
+      post.style.display = 'none';
+    } else if (positive_comments.includes(postId)){
+      console.log('already deemed positive');
+    }
+    let postContent = document.getElementsByClassName('userContent')[0].innerHTML;
+    console.log(postContent);
   })
 };
 
