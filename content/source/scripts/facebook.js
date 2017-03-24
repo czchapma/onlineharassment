4e96ad1f410b55d664ec68768e73cd60b7f657bc
Christine Liao <astridcliao@gmail.com>
2017-03-24 10:22:52 -0700

    going through each post

--- content/source/scripts/facebook.js
+++ content/source/scripts/facebook.js
@@ -6,7 +6,10 @@ const positive_comments = [];
 const checkFacebookFilter = function(state){
   const harmful_words = state.harmful_words;
   const filter_on = state.filter_on;
-
+  let posts = Array.from(document.getElementsByClassName('UserContent'));
+  posts.forEach(post => {
+    
+  })
 };
 
 export default checkFacebookFilter;
